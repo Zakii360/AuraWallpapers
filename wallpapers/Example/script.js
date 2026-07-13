444a9b24039366a1e0e72f6c1ac63e9960ff1cb5
Zakii <zakii@360-search.com>
2026-07-13 11:19:27 -0400

    Delete wallpapers directory

--- wallpapers/Example/script.js
+++
@@ -1,4 +0,0 @@
-// Wallpaper packages are plain web pages. AuraWallpapers loads this file
-// like any browser would; effects such as rain, snow or camera drift are
-// layered on top by the host application and do not need to be
-// implemented here.
